0a368a2a9387b98d69dd64a647d6a5be1bdc1ab3
Francesco <francesco.savignano96@gmail.com>
2020-10-12 16:00:10 +0200

    generatore impostato

---
+++ main.js
@@ -0,0 +1,6 @@
+var name = prompt("Inserisci il tuo nome: ");
+var surname = prompt("Inserisci il tuo cognome: ");
+var color = prompt("Inserisci il tuo colore preferito: ");
+var year = 20;
+
+document.getElementById("pswdgenerator").innerHTML = name + surname + color + year;
