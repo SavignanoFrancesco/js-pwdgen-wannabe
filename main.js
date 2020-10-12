9b2db43c01acc24d61f11d5b03ed375bd3b80e09
Francesco <francesco.savignano96@gmail.com>
2020-10-12 16:13:49 +0200

    esercizio completato

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ var surname = prompt("Inserisci il tuo cognome: ");
 var color = prompt("Inserisci il tuo colore preferito: ");
 var year = 20;
 
+document.getElementById('title').innerHTML = 'Password generata con successo!';
 document.getElementById("pswdgenerator").innerHTML = name + surname + color + year;
